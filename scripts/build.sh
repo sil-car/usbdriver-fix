16af652c25829f5f4c021d5a553cf707eff9d55e
Nate Marti <nate_marti@sil.org>
2024-06-24 19:11:27 +0100

    fix path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1090
+# shellcheck disable=SC1091
 
 # Create venv.
 env=buildenv  # has to match folder name in app.spec
 python3 -m venv "$env" || python -m venv "$env"
 # Enter venv.
-source "./$env/bin/activate"
+source "$env/bin/activate"
 # Build package.
 python -m pip install .
 # Build executable.
